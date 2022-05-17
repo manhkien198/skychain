52c799c085fc0ffd99a08cfebb4cbf3a0b9838e8
manhkien198 <manhkien1998@gmail.com>
2022-05-17 23:55:26 +0700

    update

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -23,12 +23,9 @@ export function Seo({ data }: SeoProps) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
 
-      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      
-        <meta name="twitter:image" content="/skychain_thumbnail.jpg"/>
     </Head>
   );
 }
