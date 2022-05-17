24ba4dd1e51d95069a9e192abcf47129b7884936
Hoang Quan <hongquanvn1998@gmail.com>
2022-05-17 20:54:32 +0700

    update thum

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,7 +126,7 @@ const Home = (props: Props) => {
   return (
     <Box className={classes.root}>
       <Seo data={{title:"SkyChain.Capital | MORE THAN AN INVESTOR",
-description:"Emerging into the blockchain era, we proudly claim that we are providing firm values to blockchain projects. We want to invest in all aspects of blockchain, from fields as sophisticated as DeFi to essential as Web3 or entertaining as GameFi and NFTs…As an Asia-based blockchain capital, we are aiming to build a community in which each member can be the masterpiece of their own.",
+description:"As an Asia-based blockchain capital, we are aiming to build a community in which each member can be the masterpiece of their own.",
 url:'https://skychain.capital/',
 
 
