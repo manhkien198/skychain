d35ed1e3196187f36a7238b8b773905c6d94c0c7
manhkien198 <manhkien1998@gmail.com>
2022-05-18 09:55:27 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? "https://cdn.contactform.dev" : "",
 };
 
 module.exports = nextConfig;
